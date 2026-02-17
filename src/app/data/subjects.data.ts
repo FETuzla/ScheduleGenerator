b83bc322ea57cfc26996f3adf612859cfcb8588b
SenadM02 <malkic.senad02@gmail.com>
2026-02-17 17:00:28 +0100

    initial

---
+++ src/app/data/subjects.data.ts
@@ -0,0 +1,44 @@
+import { Subject } from '../models/subject.model';
+
+export const SUBJECTS: Subject[] = [
+    {
+        id: 1,
+        name: 'Matematika II',
+        day: 'Ponedjeljak',
+        start: '13:00',
+        end: '16:00',
+        location: 'A2'
+    },
+    {
+        id: 2,
+        name: 'Osnovi Elektrotehnike II',
+        day: 'Utorak',
+        start: '11:00',
+        end: '14:00',
+        location: 'A2'
+    },
+    {
+        id: 3,
+        name: 'Fizika II',
+        day: 'Srijeda',
+        start: '14:00',
+        end: '17:00',
+        location: 'A2'
+    },
+    {
+        id: 4,
+        name: 'Osnovi programiranja',
+        day: 'Cetrvrtak',
+        start: '14:00',
+        end: '17:00',
+        location: 'A2'
+    },
+    {
+        id: 5,
+        name: 'TPTP',
+        day: 'Petak',
+        start: '11:00',
+        end: '14:00',
+        location: 'A2'
+    },
+]
---
+++ src/app/models/subject.model.ts
@@ -0,0 +1,8 @@
+export interface Subject {
+    id: number;
+    name: string;
+    day: string;
+    start: string;
+    end: string;
+    location: string;
+}
