d3bd6ccfd83e3905215929e91e81afdb65264a27
Adnan Maleskic <maleskicadnan@gmail.com>
2026-02-19 22:07:38 +0100

    Add type to lecture

--- src/app/models/schedule.model.ts
+++ src/app/models/schedule.model.ts
@@ -8,8 +8,11 @@ export const FIRST_SELECTORS = [
 
 export const SECOND_SELECTORS = ['Linija 1', 'Linija 2', 'AR', 'RI', 'ESKE', 'EEMS', 'TK'] as const;
 
+export const LECTURE_TYPES = ['Predavanje', 'AV', 'LV'] as const;
+
 export type FirstSelector = (typeof FIRST_SELECTORS)[number];
 export type SecondSelector = (typeof SECOND_SELECTORS)[number];
+export type LectureType = (typeof LECTURE_TYPES)[number];
 
 export interface Lecture {
   name: string;
@@ -18,6 +21,7 @@ export interface Lecture {
   endTime: string;
   location: string;
   teacher: string;
+	type: LectureType;
   topPercent: number;
   leftPercent: number;
   widthPercent: number;
