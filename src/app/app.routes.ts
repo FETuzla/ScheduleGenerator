f11acf8430db743e4884b515cc734a1860c43025
Adnan Maleskic <maleskicadnan@gmail.com>
2026-02-23 23:09:01 +0100

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Homepage } from './homepage/homepage'
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: Homepage
-    }
-];
